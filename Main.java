a84b69871046574e43ba134c95673c5148ffb1b0
ttdangg <ttdang1445@gmail.com>
2025-01-16 10:17:59 -0800

    finished 1 and 2

--- Main.java
+++ Main.java
@@ -4,18 +4,36 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Scanner sc = new Scanner(System.in);
     // Problem 1 - Create a scanner and an array of size 3
+    // double [] num = new double[3];
+    // System.out.println("Enter 3 numbers: ");
+    // num[0] = sc.nextDouble();
+    // num[1] = sc.nextDouble();
+    // num[2] = sc.nextDouble();
+
+    // double sum = num[0] + num[1] + num[2];
+    // System.out.println("The sum is: " + sum);
 
     // Problem 2
     // Use the scanner to get user input and give it to the method
-
-    int N = /* scan.nextInt() */;
+    System.out.println("Enter a positive number: ");
+    int N = sc.nextInt();
     int[] output = sumsOfNums(N);
-  }
+        
+    for (int i = 0; i < output.length; i++) 
+    {
+      System.out.print(output[i] + " ");
+    }
 
-  public static int[] sumsOfNums(int input)
-  {
-    // Implement Problem 2 here
-    return 0;
-  }
+    public static int[] sumsOfNums(int N) {
+        int sum = 0;
+        int[] output = new int[N];
+        for (int i = 0; i < N; i++) {
+            sum += i;
+            output[i] = sum;
+        }
+        return output;
+    }
 }
+
